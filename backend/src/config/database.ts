37a4b9dd0c32b0a50340205310f0413c4b8a9513
Jerry bony <info@techluminateacademy.com>
2025-02-24 13:23:09 -0300

    remove question mark syntax

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,7 +5,7 @@
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 dotenv.config();
-const mongoURI: string = process.env.MONGODB_URI ??;
+const mongoURI: string = process.env.MONGODB_URI;
 
 
 
